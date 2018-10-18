185f05ead866164c44a7c7293e90677db032ca6f
Tom Pritchard <tom.pritchard@paystream.co.uk>
2018-10-18 14:55:37 +0100

    added home controller and home page

--- Source/DishFinder.Web/Startup.cs
+++ Source/DishFinder.Web/Startup.cs
@@ -15,6 +15,7 @@ namespace DishFinder.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,7 +26,7 @@ namespace DishFinder.Web
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) => { await context.Response.WriteAsync("Hello World!"); });
+            app.UseMvc();
         }
     }
 }
